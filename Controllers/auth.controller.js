c87dfd9067773f449e83f18704231b1e64812004
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2023-04-21 00:42:01 +0530

    Update auth.controller.js

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -1,26 +1,26 @@
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const nodemailer = require('nodemailer')
-const {google} = require('googleapis')
+const { google } = require('googleapis')
 
 require('dotenv').config()
 
 const models = require("../database/models");
 const createError = require("http-errors");
 const bcrypt = require("bcrypt");
-const {userSchema, loginSchema} = require("../helpers/validation.schema");
-const {signAccessToken} = require("../helpers/jwt_helper");
+const { userSchema, loginSchema } = require("../helpers/validation.schema");
+const { signAccessToken } = require("../helpers/jwt_helper");
 const sequelize = require("sequelize");
 
-const CLIENT_ID=process.env.GOOGLECLIENT_ID
-const CLIENT_SECRET=process.env.CLIENT_SECRET
-const REDIRECT_URI=process.env.REDIRECT_URI
-const REFRESH_TOKEN=process.env.GOOGLEREFRESH_TOKEN
+const CLIENT_ID = process.env.GOOGLECLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const REDIRECT_URI = process.env.REDIRECT_URI
+const REFRESH_TOKEN = process.env.GOOGLEREFRESH_TOKEN
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
-oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN})
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
 
-exports.registerUser = async(req, res, next) => {
+exports.registerUser = async (req, res, next) => {
     try {
 
         const result = await userSchema.validateAsync(req.body)
@@ -31,73 +31,75 @@ exports.registerUser = async(req, res, next) => {
             }
         });
 
-        if(user) throw createError.Conflict(`${req.body.email} is already registered`)
+        if (user) throw createError.Conflict(`${req.body.email} is already registered`)
 
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(result.password, salt)
         result.password = hashedPassword
 
-        const savedUser = await models.User.create(result)
-        const accessToken = await signAccessToken(savedUser.email)
+        if (verifyEmail(email)) {
 
-        const accessTokenUpdate = await models.User.update(
-            {accessTokens: [accessToken]},
-            {where: {email: result.email}}
-        )
+            const savedUser = await models.User.create(result)
+            const accessToken = await signAccessToken(savedUser.email)
 
-        res.status(201).send({ message: "User registered successfully", accessToken: accessToken})
+            const accessTokenUpdate = await models.User.update(
+                { accessTokens: [accessToken] },
+                { where: { email: result.email } }
+            )
 
-    } catch(error) {
+            res.status(201).send({ message: "User registered successfully", accessToken: accessToken })
+        }
+    } catch (error) {
 
-        if(error.isJoi === true) error.status = 422;
+        if (error.isJoi === true) error.status = 422;
 
         next(error)
     }
 }
 
-exports.loginUser = async(req, res, next) => {
+exports.loginUser = async (req, res, next) => {
     try {
 
         const result = await loginSchema.validateAsync(req.body)
-        
+
         const user = await models.User.findOne({
-            where : {
+            where: {
                 email: result.email
             }
         })
 
-        if(!user) throw createError.BadRequest(`Invalid Username or Password`)
+        if (!user) throw createError.BadRequest(`Invalid Username or Password`)
 
         var passwordFlag = await bcrypt.compare(result.password, user.password)
 
-        if(!passwordFlag) throw createError.BadRequest(`Invalid Username or Password`)
+        if (!passwordFlag) throw createError.BadRequest(`Invalid Username or Password`)
 
         var userAccessTokens = user.accessTokens
         console.log(userAccessTokens.length)
 
-        if(userAccessTokens.length>=4) throw createError.BadRequest(`Too many logins`);
+        if (userAccessTokens.length >= 4) throw createError.BadRequest(`Too many logins`);
 
         const accessToken = await signAccessToken(user.email)
 
         const accessTokenUpdate = await models.User.update(
-            {accessTokens: sequelize.fn('array_cat', sequelize.col('accessTokens'), [accessToken])},
-            {where: {email: result.email}}
+            { accessTokens: sequelize.fn('array_cat', sequelize.col('accessTokens'), [accessToken]) },
+            { where: { email: result.email } }
         )
 
         res.status(201).send({ message: "Login successful", accessToken: accessToken })
 
-    } catch(error) {
+    } catch (error) {
 
-        if(error.isJoi === true) return next(createError.BadRequest("Invalid Username or Password"));
+        if (error.isJoi === true) return next(createError.BadRequest("Invalid Username or Password"));
 
         next(error)
 
     }
 }
 
-exports.logoutUser = async(req, res, next) => {
+exports.logoutUser = async (req, res, next) => {
     try {
-        
+
         const accessToken = req.body.accessToken;
 
         const accessTokenDetails = JSON.parse(Buffer.from(accessToken.split('.')[1], 'base64').toString('ascii'))
@@ -105,42 +107,42 @@ exports.logoutUser = async(req, res, next) => {
         console.log(email)
 
         const user = await models.User.findOne({
-            where : {
+            where: {
                 email: email
             }
         })
 
-        if(user.accessTokens.includes(accessToken) == false) throw createError.BadRequest(`Invalid request`);
+        if (user.accessTokens.includes(accessToken) == false) throw createError.BadRequest(`Invalid request`);
 
         const accessTokenDelete = await models.User.update(
-            {accessTokens: sequelize.fn('array_remove', sequelize.col('accessTokens'), accessToken)},
-            {where: {email: email}}
+            { accessTokens: sequelize.fn('array_remove', sequelize.col('accessTokens'), accessToken) },
+            { where: { email: email } }
         )
 
         res.status(201).send({ message: "Logout successful" })
-    
-    } catch(error) {
+
+    } catch (error) {
         next(error)
     }
 }
 
-exports.forgotPassword = async(req, res, next) => {
+exports.forgotPassword = async (req, res, next) => {
     try {
 
         const email = req.body.email;
 
         const user = await models.User.findOne({
-            where : {
+            where: {
                 email: email
             }
         })
 
-        if(!user) throw createError.BadRequest(`Email ID not registered`)
+        if (!user) throw createError.BadRequest(`Email ID not registered`)
 
         // OTP
 
-        const generateRandomString = function (length=8) {
-            return Math.random().toString(20).slice(2, 2+length)
+        const generateRandomString = function (length = 8) {
+            return Math.random().toString(20).slice(2, 2 + length)
         }
 
         const otp = generateRandomString()
@@ -149,13 +151,15 @@ exports.forgotPassword = async(req, res, next) => {
         const hashedOtp = await bcrypt.hash(otp, salt)
 
         const userUpdate = await models.User.update(
-            {otp: hashedOtp,
-            otpTimestamp: Date()},
-            {where: {email: email}}
+            {
+                otp: hashedOtp,
+                otpTimestamp: Date()
+            },
+            { where: { email: email } }
         )
 
         //
-        
+
         async function sendMail() {
 
             try {
@@ -174,61 +178,61 @@ exports.forgotPassword = async(req, res, next) => {
                 })
 
                 const mailOptions = {
-                    from:   process.env.USEREMAILID,
+                    from: process.env.USEREMAILID,
                     to: email,
-                    subject: 'Password Reset',
-                    text: 'OTP to reset password',
-                    html: '<p>Your OTP is <b>'+otp+'</b></p>'
+                    subject: 'Email Verification',
+                    text: 'OTP for email verification',
+                    html: '<p>Your OTP is <b>' + otp + '</b></p>'
                 }
 
                 const result = await transport.sendMail(mailOptions)
                 return result
 
-            } catch(error) {
+            } catch (error) {
                 next(error)
             }
         }
 
         sendMail()
-        .then((result) => res.status(201).send({ message: "OTP is sent to your email ID" }))
-        .catch((error) => next(error))
+            .then((result) => res.status(201).send({ message: "OTP is sent to your email ID" }))
+            .catch((error) => next(error))
 
-    } catch(error) {
+    } catch (error) {
         next(error)
     }
 }
 
-exports.approveOtp = async(req, res, next) => {
+exports.approveOtp = async (req, res, next) => {
     try {
 
         const email = req.body.email;
         const otp = req.body.otp;
 
         const user = await models.User.findOne({
-            where : {
+            where: {
                 email: email
             }
         })
 
-        if(!user) throw createError.BadRequest(`Email ID not registered`)
+        if (!user) throw createError.BadRequest(`Email ID not registered`)
 
         var otpFlag = await bcrypt.compare(otp, user.otp)
 
-        if(!otpFlag) throw createError.BadRequest(`Invalid OTP`)
+        if (!otpFlag) throw createError.BadRequest(`Invalid OTP`)
 
         var diff = new Date() - user.otpTimestamp
-        
-        if((diff/(1000*60)) > 5) throw createError.BadRequest(`OTP expired`)
-    
+
+        if ((diff / (1000 * 60)) > 5) throw createError.BadRequest(`OTP expired`)
+
         res.status(201).send({ message: "OTP verified" })
 
 
-    } catch(error) {
+    } catch (error) {
         next(error)
     }
 }
 
-exports.resetPassword = async(req, res, next) => {
+exports.resetPassword = async (req, res, next) => {
     try {
 
         const email = req.body.email
@@ -238,43 +242,43 @@ exports.resetPassword = async(req, res, next) => {
         const hashedPassword = await bcrypt.hash(password, salt)
 
         const userPasswordUpdate = await models.User.update(
-            {password: hashedPassword},
-            {where: {email: email}}
+            { password: hashedPassword },
+            { where: { email: email } }
         )
 
         res.status(201).send({ message: "Password reset successful" })
 
-    } catch(error) {
+    } catch (error) {
         next(error)
     }
 }
 
-exports.logoutAll = async(req, res, next) => {
+exports.logoutAll = async (req, res, next) => {
     try {
 
         const email = req.body.email
 
         const user = await models.User.findOne({
-            where : {
+            where: {
                 email: email
             }
         })
 
-        if(!user) throw createError.BadRequest(`Email ID not registered`)
+        if (!user) throw createError.BadRequest(`Email ID not registered`)
 
         const accessTokenUpdate = await models.User.update(
-            {accessTokens: []},
-            {where: {email: email}}
+            { accessTokens: [] },
+            { where: { email: email } }
         )
 
         res.status(201).send({ message: "Logout successful from all devices" })
 
-    } catch(error) {
+    } catch (error) {
         next(error)
     }
 }
 
-exports.changePassword = async(req, res, next) => {
+exports.changePassword = async (req, res, next) => {
     try {
 
         const accessToken = req.body.accessToken;
@@ -283,28 +287,103 @@ exports.changePassword = async(req, res, next) => {
 
         const accessTokenDetails = JSON.parse(Buffer.from(accessToken.split('.')[1], 'base64').toString('ascii'))
         email = accessTokenDetails.aud
-        
+
         const user = await models.User.findOne({
-            where : {
+            where: {
                 email: email
             }
         })
 
         var passwordFlag = await bcrypt.compare(oldPassword, user.password)
 
-        if(!passwordFlag) throw createError.BadRequest(`Incorrect Password`)
+        if (!passwordFlag) throw createError.BadRequest(`Incorrect Password`)
 
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(newPassword, salt)
 
         const passwordUpdate = await models.User.update(
-            {password: hashedPassword},
-            {where: {email: email}}
+            { password: hashedPassword },
+            { where: { email: email } }
         )
 
-        res.status(201).send({ message: "Password Updated Successfully"})
+        res.status(201).send({ message: "Password Updated Successfully" })
 
-    } catch(error) {
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.verifyEmail = async (email) => {
+    try {
+        // const email = req.body.email;
+
+        // const user = await models.User.findOne({
+        //     where: {
+        //         email: email
+        //     }
+        // })
+
+        // if (!user) throw createError.BadRequest(`Email ID not registered`)
+
+        // OTP
+
+        const generateRandomString = function (length = 8) {
+            return Math.random().toString(20).slice(2, 2 + length)
+        }
+
+        const otp = generateRandomString()
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedOtp = await bcrypt.hash(otp, salt)
+
+        const userUpdate = await models.User.update(
+            {
+                otp: hashedOtp,
+                otpTimestamp: Date()
+            },
+            { where: { email: email } }
+        )
+
+        //
+
+        async function sendMail() {
+
+            try {
+                const accessToken = await oAuth2Client.getAccessToken()
+
+                const transport = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        type: 'OAuth2',
+                        user: process.env.USEREMAILID,
+                        clientId: CLIENT_ID,
+                        clientSecret: CLIENT_SECRET,
+                        refreshToken: REFRESH_TOKEN,
+                        accessToken: accessToken
+                    }
+                })
+
+                const mailOptions = {
+                    from: process.env.USEREMAILID,
+                    to: email,
+                    subject: 'Password Reset',
+                    text: 'OTP to reset password',
+                    html: '<p>Your OTP is <b>' + otp + '</b></p>'
+                }
+
+                const result = await transport.sendMail(mailOptions)
+                return result
+
+            } catch (error) {
+                next(error)
+            }
+        }
+
+        sendMail()
+            .then((result) => res.status(201).send({ message: "OTP is sent to your email ID" }))
+            .catch((error) => next(error))
+
+    } catch (error) {
         next(error)
     }
 }
